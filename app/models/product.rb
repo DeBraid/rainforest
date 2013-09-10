99691a42c800d9fa795f724e95bf2f196656362b
DeBraid <de.braid@gmail.com>
2013-09-10 18:14:50 -0400

    validations: ensure user passes proper info

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+  attr_accessible :description, :name, :price_in_cents
+
+  validates :description, :name, :presence => true
+  validates :price_in_cents, :numericity => {:only_integer => true}
 end
