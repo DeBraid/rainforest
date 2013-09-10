0c759c33eb84c22e4a80a76d6b384576c6dab827
DeBraid <de.braid@gmail.com>
2013-09-10 17:56:35 -0400

    creating basic API by refactoring to add JSON response in prod cont

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,14 +2,29 @@ class ProductsController < ApplicationController
   
   def index
     @products = Product.all
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @products }
+    end
   end
 
   def show
     @product = Product.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @product }
+    end
   end
 
   def new
     @product = Product.new
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @product }
+    end
   end
 
   def edit
@@ -19,31 +34,43 @@ class ProductsController < ApplicationController
   def create
     @product = Product.create(product_params)
 
-    if @product.save
-      redirect_to products_url
-    else
-      render :new
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created!'}
+        format.json { render json: @product, status: :created, location: @product} 
+      else
+        format.html { render action: "new" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def update
     @product = Product.find(params[:id])
 
-    if @product.update_attributes(product_params)
-      redirect_to product_path(@product)
-    else
-      render :edit
+    respond_to do |format|
+      if @product.update_attributes(product_params)
+        format.html { redirect_to @product, notice: 'Product Update Successful.'}
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit'}
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
     end
   end
   
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
-    redirect_to product_path
+
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { had :no_content }  
+    end
   end
 
-  def product_params
-    params.require(:product).permit(:name, :description, :price_in_cents)
-  end
   
+    def product_params
+      params.require(:product).permit(:name, :description, :price_in_cents)
+    end  
 end
